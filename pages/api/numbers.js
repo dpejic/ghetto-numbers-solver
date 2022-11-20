9d8b6badd6820f272343ee2a90b961fc581595cc
Damjan <dakipjc2@gmail.com>
2022-11-21 00:01:26 +0100

    Initial commit :tada:

---
+++ pages/api/numbers.js
@@ -0,0 +1,8 @@
+import { solutions } from "../../utils/helpers/numbers";
+
+export default async function handler(req, res) {
+  const { target, numbers } = req.query;
+  const numbersArray = numbers.split(",").map((number) => +number);
+  const exp = await solutions(+target, numbersArray);
+  res.json({ target, exp: exp.toString() });
+}
