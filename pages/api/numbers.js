f3834ecd5ca257967f34aa1061141a19499ae81e
Damjan <dakipjc2@gmail.com>
2022-11-21 01:53:24 +0100

    Added option for negative numbers

--- pages/api/numbers.js
+++ pages/api/numbers.js
@@ -1,8 +1,8 @@
 import { solutions } from "../../utils/helpers/numbers";
 
 export default async function handler(req, res) {
-  const { target, numbers, position } = req.query;
+  const { target, numbers, position, negative } = req.query;
   const numbersArray = numbers.split(",").map((number) => +number);
   const exp = await solutions(+target, numbersArray);
-  res.json({ target, exp: exp.toString(), position });
+  res.json({ target, exp: exp.toString(), position, negative });
 }
